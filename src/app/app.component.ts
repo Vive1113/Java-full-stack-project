e95f5f17f15134d129053d12f14be9c502ce4f7f
Vive1113 <120009949+Vive1113@users.noreply.github.com>
2023-04-24 18:45:23 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { AuthenticationService } from './authentication.service';
   styleUrls: ['./app.component.css']
   
 })
+
+//app component is the root component of any angular project.
+//here, constructor of this class is created with paramters of router and authentication service
+//confirmLogout method is created to show popup and get confirmation for logging out
 export class AppComponent {
   title = 'Student Management';
   constructor(public loginService:AuthenticationService) { }
