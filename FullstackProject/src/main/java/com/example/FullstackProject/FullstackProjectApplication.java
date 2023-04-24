21ee8d494d8634fb089a59b83465f59d3f7473a3
Vive1113 <120009949+Vive1113@users.noreply.github.com>
2023-04-24 18:37:53 +0530

    Update FullstackProjectApplication.java

--- FullstackProject/src/main/java/com/example/FullstackProject/FullstackProjectApplication.java
+++ FullstackProject/src/main/java/com/example/FullstackProject/FullstackProjectApplication.java
@@ -3,6 +3,11 @@ package com.example.FullstackProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/* In this class @SpringBootApplication annotation is used.
+ * It is used to mark a configuration class that declares one or more @Bean methods and 
+ * also triggers auto-configuration and component scanning.
+ * It's same as declaring a class with @Configuration, @EnableAutoConfiguration and @ComponentScan annotations.*/
 @SpringBootApplication
 public class FullstackProjectApplication {
 
