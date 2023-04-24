76954ed5c69fcdc1e329c99694d57824fd1e4447
Vive1113 <120009949+Vive1113@users.noreply.github.com>
2023-04-24 18:28:54 +0530

    Update Repository.java

--- FullstackProject/src/main/java/com/example/FullstackProject/Repository/Repository.java
+++ FullstackProject/src/main/java/com/example/FullstackProject/Repository/Repository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.FullstackProject.Student.Student;
 
+/* This Interface extends JpaRepository interface, so that
+ *  @Repository is not needed to be annotated manually */
 @org.springframework.stereotype.Repository
 public interface Repository extends JpaRepository<Student, Integer>{
 	List<Student> findByStudentNameContaining(String studentName);
