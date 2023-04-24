54e85d84128bab4264d6bf402e11acb8bdbbde71
Vive1113 <120009949+Vive1113@users.noreply.github.com>
2023-04-24 18:27:31 +0530

    Update ResourceNotFound.java

--- FullstackProject/src/main/java/com/example/FullstackProject/ExceptionHandling/ResourceNotFound.java
+++ FullstackProject/src/main/java/com/example/FullstackProject/ExceptionHandling/ResourceNotFound.java
@@ -1,7 +1,10 @@
 package com.example.FullstackProject.ExceptionHandling;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+/* @ResponseStatus marks a exception class with the status code and reason message that should be returned. 
+ * The status code is applied to the HTTP response when the handler method is invoked, 
+ * or whenever the specified exception is thrown.
+ * In this class, constructor injection is used to access parent class constructor*/
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFound extends RuntimeException{
 
